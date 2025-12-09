b1e06a2f135c7ac35f58ab982ead8e517534cb91
ganesh charan paturu.v.n.s <ganesh@test.com>
2025-12-09 22:58:50 +0530

    added search and display operations

--- data.js
+++
@@ -1,7 +0,0 @@
-export  const data=[
-    {id:"AP24110011724" ,name: "Gunithi kishor"           , cgpa: 9.66},
-    {id:"AP24110011729" ,name:"anand gokul kota"          ,cgpa:9.0},
-    {id:"AP24110011685" ,name:"Dogga yashwant"            ,cgpa:9.26},
-    {id:"AP24110011694" ,name:"Reddy Nithin"              ,cgpa:9.4},
-    {id:"AP24110011704" ,name:"Paturu v N S Ganesh Charan",cgpa:9.36}
-];
